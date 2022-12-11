b1f17adbbfe7b71f3d56dc356e5ebc3633b14c7e
lukenguyen197 <113011455+lukenguyen197@users.noreply.github.com>
2022-12-11 10:09:31 +0400

    index

---
+++ portfolio/script.js
@@ -0,0 +1,23 @@
+// This is to set up the slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+// This function is to maintain that the images transition from one to another in the slideshow
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
